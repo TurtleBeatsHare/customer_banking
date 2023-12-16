ad75c3988701d047f0aec9bb37518204dc95323b
TurtleBeatsHare <adammillington@Adams-MacBook-Pro.local>
2023-12-16 02:49:15 -0700

    Adjusted input_validation list. Added adjustment to True in first section of Try statement:

--- customer_banking.py
+++ customer_banking.py
@@ -11,42 +11,50 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     print("-" * 72)
-    valid_input = {'Savings Balance': False, 'Savings Interest': False, 'Savings Maturity': False}
+    input_validation = {'Savings Balance': False, 'Savings Interest': False, 'Savings Maturity': False}
 
     while True:
         try:
+            # Check if variable exists and is a float
             float(savings_balance)
+            input_validation['Savings Balance'] = True
         except:
             try:
+                # If previous check does not pass acquire input and convert to float
                 savings_balance = float(input("Please input the savings account initial balance:  "))
-                valid_input['Savings Balance'] = True
+                # If previous executes correctly, input has been received and is a float
+                input_validation['Savings Balance'] = True
             except:
+                # If the input above produced error, leave input_validation False and pass
+                # Option to retry is offered after all three inputs for this section
                 pass
         
         try:
             float(savings_interest)
+            input_validation['Savings Interest'] = True
         except:
             try:
                 savings_interest = float(input("Please input the savings account interest rate (apr):  "))
-                valid_input['Savings Interest'] = True
+                input_validation['Savings Interest'] = True
             except:
                 pass
 
         try:
             int(savings_maturity)
+            input_validation['Savings Maturity'] = True
         except:
             try:
                 savings_maturity = int(input("Please input the esitmated savings account duration in months:  "))
-                valid_input['Savings Maturity'] = True
+                input_validation['Savings Maturity'] = True
             except:
                 pass       
         
         
-        if all(valid_input.values()):
+        if all(input_validation.values()):
             break
         else:
             print("\n")
-            [print(f"The input for {key} was invalid.") for (key, value) in valid_input.items() if not value]
+            [print(f"The input for {input_key} was invalid.") for (input_key, valid_input) in input_validation.items() if not valid_input]
             if input("Would you like to try again? (Y/N):  ").lower() in ["y", "yes"]:
                 print("\n")
             else:
@@ -62,42 +70,45 @@ def main():
 
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-    valid_input = {'CD Balance': False, 'CD Interest': False, 'CD Maturity': False}
+    input_validation = {'CD Balance': False, 'CD Interest': False, 'CD Maturity': False}
 
     while True:
         try:
             float(cd_balance)
+            input_validation['CD Balance'] = True
         except:
             try:
                 cd_balance = float(input("Please input the cd account initial balance:  "))
-                valid_input['CD Balance'] = True
+                input_validation['CD Balance'] = True
             except:
                 pass
         
         try:
             float(cd_interest)
+            input_validation['CD Interest'] = True
         except:
             try:
                 cd_interest = float(input("Please input the cd account interest rate (apr):  "))
-                valid_input['CD Interest'] = True
+                input_validation['CD Interest'] = True
             except:
                 pass
 
         try:
             int(cd_maturity)
+            input_validation['CD Maturity'] = True
         except:
             try:
                 cd_maturity = int(input("Please input the esitmated cd account duration in months:  "))
-                valid_input['CD Maturity'] = True
+                input_validation['CD Maturity'] = True
             except:
                 pass       
         
         
-        if all(valid_input.values()):
+        if all(input_validation.values()):
             break
         else:
             print("\n")
-            [print(f"The input for {key} was invalid.") for (key, value) in valid_input.items() if not value]
+            [print(f"The input for {input_key} was invalid.") for (input_key, valid_input) in input_validation.items() if not valid_input]
             if input("Would you like to try again? (Y/N):  ").lower() in ["y", "yes"]:
                 print("\n")
             else:
